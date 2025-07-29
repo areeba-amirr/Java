7fe40fdb5ab99668031663d169b216e2c710a55f
areeba-amirr <areebaamir826@gmail.com>
2025-07-29 13:53:11 +0500

    Varaibles and Primitive Data Types

---
+++ VariablesDatatypes.java
@@ -0,0 +1,21 @@
+public class VariablesDatatypes {
+      public static void main(String[] args) {
+        System.err.println("*********Variables and DataTypes in JAVA**********");
+     
+        int age; //declaration
+        age = 10; //initialization
+        System.out.println(age);
+        //or
+        // System.out.print(age);
+        char letter = 'a'; //always enclosed in single quotes
+        System.out.println(letter);
+        float height = 5.5f; //always ends with f
+        System.out.println(height);
+        double value = 445.88d;
+        System.out.println(value);
+        long big = 6668887666666666668L;
+        System.out.println(big);
+    }
+}
+
+ 
