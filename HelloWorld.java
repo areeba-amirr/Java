e9407453018b89a04b2d50733e97b441a29f8f65
areeba-amirr <areebaamir826@gmail.com>
2025-07-29 19:47:36 +0500

    First Program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld { //Pascal Convention is use for class name
     public static void main(String[] args) { //for function's name we use Camel Convention
-        System.out.println("Hello World!!!!!!!!,Areeba here");
+        System.out.println("Hello World!!!!!!!!git,Areeba here");
     }
 }
