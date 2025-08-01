1fd274f2ef5d676f960c8ac4db639a473fde05c6
areeba-amirr <areebaamir826@gmail.com>
2025-08-01 16:43:43 +0500

    Loop

---
+++ Loops.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Loops {
+    public static void main(String[] args){
+        //While Loop
+        Scanner obj = new Scanner(System.in);
+        int num = obj.nextInt();
+        // int i = 1; //initialization 
+        // while(i <= num){ //condition
+        //     System.out.printf("%d\n",i);
+        //     i++; //updation
+        // }
+        //Do While Loop
+        // int i = 1; //initialization
+        // do{
+        //     System.out.printf("%d\n",i); 
+        //     i++; //updation
+        // }
+        // while(i <= num); //condition
+        //For Loop
+        for(int i = 1; i <= num; i++){
+            System.out.printf("%d\n",i);
+        }
+    }
+}
