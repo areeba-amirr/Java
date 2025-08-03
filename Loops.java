7749d60627b81b300842728f5788338fc8981de7
areeba-amirr <areebaamir826@gmail.com>
2025-08-03 12:40:57 +0500

    Loops and Nested Loops

--- Loops.java
+++ Loops.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Loops {
     public static void main(String[] args){
         //While Loop(Pre Testing Loop)
-        Scanner obj = new Scanner(System.in);
-        int num = obj.nextInt();
+        // Scanner obj = new Scanner(System.in);
+        // int num = obj.nextInt();
         // int i = 1; //initialization 
         // while(i <= num){ //condition
         //     System.out.printf("%d\n",i);
@@ -18,9 +18,45 @@ public class Loops {
         // while(i <= num); //condition
         //For Loop (Definite Loop)
         //intialization;condition;updation
-        for(int i = 1; i <= num; i++){
-            System.out.printf("%d\n",i);
+        // for(int i = 1; i <= num; i++){
+        //     System.out.printf("%d\n",i);
+        // }
+        //BREAK
+        // for(int i = 1;i <= 10; i++){
+        //     if (i == 6) break; //terminate the loop
+        //     System.out.println(i);
+        // }
+        // System.out.println("Loop End");
+        //Continue
+        //  for(int i = 1;i <= 10; i++){
+        //     if (i == 4) continue; //only skip this iteration
+        //      System.out.println(i);
+        // }
+        // System.out.println("Loop End");
+        //*****************NESTED LOOPS**************
+        // for(int i=1;i <= 3;i++){ //outer loop
+        //     for(int j=1;j <= 3;j++){ //inner loop
+        //         System.out.print(i + " " + j + " ");
+        //     }
+        // }
+        // for(int i=1;i <= 3;i++){
+        //     for(int j = 1;j <= 3;j++){
+        //         if(i == 2 && j == 2){
+        //             break;
+        //         }
+        //         System.out.println(i + " " + j);
+        //     }
+        // }
+         outside: for(int i=1;i <= 3;i++){
+           inside: for(int j = 1;j <= 3;j++){
+                if(i == 2 && j == 2){
+                    // break outside;
+                     break inside;
+                }
+                System.out.println(i + " " + j);
+            }
         }
+
     }
 }
  
