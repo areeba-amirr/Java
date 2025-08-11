3385762b9cb3f1acd438742eae1188e22206d6d3
areeba-amirr <areebaamir826@gmail.com>
2025-08-11 16:12:31 +0500

    Inheritance in different package

--- Cat.java
+++ Cat.java
@@ -4,15 +4,17 @@ public class Cat extends Animal{
     public void voice(){
         System.out.println("MEOW");
     }
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Cat cObj = new Cat();
         cObj.voice();
         cObj.eat();
+        
         // cObj.no1 = 1; //error beacuse access modifier is private
         cObj.no2 = 1;
         cObj.no4 = 2;
         // cObj.no3 = 3; //error beacuse access modifier is default
         cObj.display(cObj.no2,cObj.no4);
+
         //inherit getters and setters from Animal Class
         cObj.setN1(10);
         cObj.setN2(20);
