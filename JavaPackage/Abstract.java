3ed75acd6c2f8dacae45275a9785f7d89e67d3a9
areeba-amirr <areebaamir826@gmail.com>
2025-08-12 17:30:56 +0500

    Abstract Method And Class

---
+++ JavaPackage/Abstract.java
@@ -0,0 +1,37 @@
+package JavaPackage;
+/*For Abstract Methods
+ 
+public abstract returnType methodName (parameter list);
+                    or
+abstract public returnType methodName (parameter list); (better approach)
+
+For Abstract Clsass
+abstract class classNasmre{}
+ */
+abstract class Vehicle{
+    abstract public void start();
+}
+class car1 extends Vehicle{
+    @Override
+     public void start(){
+        System.out.println("Car Starts");
+     }
+}
+class bike1 extends Vehicle{
+    @Override
+    public void start(){
+        System.out.println("Bike Starts");
+     }
+}
+public class Abstract {
+    public static void main(String[] args) {
+      car1 cObj = new car1();
+      cObj.start();
+      bike1 bObj = new bike1();
+      bObj.start();
+       Vehicle vObj = new car1();
+       vObj.start();
+       Vehicle vObj1 = new bike1();
+       vObj1.start();
+}
+}
